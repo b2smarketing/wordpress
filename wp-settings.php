f15adb32ac01def5735dfd6bd1727667d454d8ce
b2smarketing <valente@b2smarketing.com>
2020-09-08 20:11:42 +0000

    08-09-2020_20:11:42

--- wp-settings.php
+++ wp-settings.php
@@ -44,6 +44,7 @@ require ABSPATH . WPINC . '/class-wp-recovery-mode-key-service.php';
 require ABSPATH . WPINC . '/class-wp-recovery-mode-link-service.php';
 require ABSPATH . WPINC . '/class-wp-recovery-mode-email-service.php';
 require ABSPATH . WPINC . '/class-wp-recovery-mode.php';
+require ABSPATH . WPINC . '/class-widget.php';
 require ABSPATH . WPINC . '/error-protection.php';
 require ABSPATH . WPINC . '/default-constants.php';
 require_once ABSPATH . WPINC . '/plugin.php';
